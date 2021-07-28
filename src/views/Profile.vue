4b3fa65d3ef83e547702306f23de1e00d5f9792c
AbdullahAltwaim <a.altwaim@safcsp.org.sa>
2021-07-28 11:34:00 +0300

    first commit

---
+++ src/views/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <div>number{{ phoneNumber }}</div>
+    <button @click="goBack">Go back</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      phoneNumber: "022222",
+    };
+  },
+  methods: {
+    goBack() {
+      window.history.length > 1 ? this.$router.go(-1) : this.$router.push("/");
+    },
+  },
+};
+</script>
